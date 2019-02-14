347d1d50b2da654d21dee1a7cde90e30911898f1
mameili <mameili@shinetechchina.com>
2019-02-14 11:10:17 +0800

    add All for Type

--- java/ESC20/src/main/java/com/esc20/service/LeaveRequestService.java
+++ java/ESC20/src/main/java/com/esc20/service/LeaveRequestService.java
@@ -2,6 +2,7 @@ package com.esc20.service;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -22,62 +23,70 @@ import com.esc20.nonDBModels.AppLeaveRequest;
 import com.esc20.nonDBModels.Code;
 import com.esc20.nonDBModels.LeaveInfo;
 import com.esc20.nonDBModels.LeaveParameters;
-import com.esc20.nonDBModels.LeaveRequestComment;
 
 @Service
 public class LeaveRequestService {
 
-    private Logger logger = LoggerFactory.getLogger(LeaveRequestService.class);
+	private Logger logger = LoggerFactory.getLogger(LeaveRequestService.class);
 
-    @Autowired
-    private LeaveRequestDao leaveRequestDao;
-    @Autowired
-    private AlertDao alertDao;   
-    
-    public BeaEmpLvRqst getleaveRequestById(int id) {
-    	return leaveRequestDao.getleaveRequestById(id);
-    }
-    public BeaEmpLvRqst saveLeaveRequest(BeaEmpLvRqst request, boolean isUpdate) {
-    	return leaveRequestDao.saveLeaveRequest(request,isUpdate);
-    }
-    public boolean deleteLeaveRequest(BeaEmpLvRqst request) {
-    	return leaveRequestDao.DeleteLeaveRequest(request);
-    }
-    
-    public List<AppLeaveRequest> getLeaveRequests(AppLeaveRequest request,String empNbr, String freq) {
-    	List<AppLeaveRequest> leaves = leaveRequestDao.getLeaveRequests(request,empNbr,freq);
-    	for(int i=0;i<leaves.size();i++) {
-    		leaves.get(i).setComments(leaveRequestDao.getLeaveComments(leaves.get(i).getId()));
-    	}
-    	return leaves;
-    }
+	@Autowired
+	private LeaveRequestDao leaveRequestDao;
+	@Autowired
+	private AlertDao alertDao;
+
+	public BeaEmpLvRqst getleaveRequestById(int id) {
+		return leaveRequestDao.getleaveRequestById(id);
+	}
+
+	public BeaEmpLvRqst saveLeaveRequest(BeaEmpLvRqst request, boolean isUpdate) {
+		return leaveRequestDao.saveLeaveRequest(request, isUpdate);
+	}
+
+	public boolean deleteLeaveRequest(BeaEmpLvRqst request) {
+		return leaveRequestDao.DeleteLeaveRequest(request);
+	}
+
+	public List<AppLeaveRequest> getLeaveRequests(AppLeaveRequest request, String empNbr, String freq) {
+		List<AppLeaveRequest> leaves = leaveRequestDao.getLeaveRequests(request, empNbr, freq);
+		for (int i = 0; i < leaves.size(); i++) {
+			leaves.get(i).setComments(leaveRequestDao.getLeaveComments(leaves.get(i).getId()));
+		}
+		return leaves;
+	}
+
 	public LeaveParameters getLeaveParameters() {
 		return leaveRequestDao.getLeaveParameters();
 	}
+
 	public List<Code> getAvailableFrequencies(String empNbr) {
 		return leaveRequestDao.getAvailableFrequencies(empNbr);
 	}
+
 	public String getFirstLineSupervisor(String empNbr, boolean usePMIS) {
-		String firstLineSupervisor=null;
+		String firstLineSupervisor = null;
 		String directReportEmployeeNumber = empNbr;
-		
-		while (firstLineSupervisor==null && directReportEmployeeNumber!=null) {
-			BhrPmisPosCtrl employeeSupervisorPMISData=null;
+
+		while (firstLineSupervisor == null && directReportEmployeeNumber != null) {
+			BhrPmisPosCtrl employeeSupervisorPMISData = null;
 			if (usePMIS) {
 				employeeSupervisorPMISData = leaveRequestDao.getEmployeeSupervisorPMISData(directReportEmployeeNumber);
-				if (employeeSupervisorPMISData==null) {
+				if (employeeSupervisorPMISData == null) {
 					return null;
 				}
-				firstLineSupervisor = leaveRequestDao.getPMISFirstLineSupervisor(employeeSupervisorPMISData.getSpvsrBilletNbr(), employeeSupervisorPMISData.getSpvsrPosNbr(), true);
+				firstLineSupervisor = leaveRequestDao.getPMISFirstLineSupervisor(
+						employeeSupervisorPMISData.getSpvsrBilletNbr(), employeeSupervisorPMISData.getSpvsrPosNbr(),
+						true);
 			} else {
-				firstLineSupervisor = leaveRequestDao.getFirstLineSupervisor(directReportEmployeeNumber, true);	
+				firstLineSupervisor = leaveRequestDao.getFirstLineSupervisor(directReportEmployeeNumber, true);
 			}
 			if (firstLineSupervisor == null) {
 				return null;
 			} else if (empNbr.equals(firstLineSupervisor)) {
 				firstLineSupervisor = null;
 				if (usePMIS) {
-					firstLineSupervisor = leaveRequestDao.getPMISFirstLineSupervisor(employeeSupervisorPMISData.getSpvsrBilletNbr(), employeeSupervisorPMISData.getSpvsrPosNbr(), false);
+					firstLineSupervisor = leaveRequestDao.getPMISFirstLineSupervisor(
+							employeeSupervisorPMISData.getSpvsrBilletNbr(), employeeSupervisorPMISData.getSpvsrPosNbr(),
+							false);
 				} else {
 					firstLineSupervisor = leaveRequestDao.getFirstLineSupervisor(directReportEmployeeNumber, false);
 				}
@@ -85,66 +94,81 @@ public class LeaveRequestService {
 					directReportEmployeeNumber = firstLineSupervisor;
 					firstLineSupervisor = null;
 				} else {
-					directReportEmployeeNumber =null;
+					directReportEmployeeNumber = null;
 				}
 			}
 		}
 		return firstLineSupervisor;
 	}
+
 	public List<Code> getAbsRsns(String empNbr, String freq, String leaveType) {
 		return leaveRequestDao.getAbsenceReasons(empNbr, freq, leaveType);
 	}
+
 	public List<Code> getLeaveTypes(String empNbr, String freq, String leaveType) {
-		return leaveRequestDao.getLeaveTypes(empNbr, freq, leaveType);
+		List<Code> res = new ArrayList<>();
+		Code code = new Code("0", "All");
+		res.add(code);
+		List<Code> codes = leaveRequestDao.getLeaveTypes(empNbr, freq, leaveType);
+		res.addAll(codes);
+		return res;
 	}
+
 	public List<LeaveInfo> getLeaveInfo(String empNbr, String freq, boolean removeZeroedOutLeaveTypes) {
-		List<LeaveInfo> leaveInfos = leaveRequestDao.getLeaveInfo(empNbr,freq);
-		
+		List<LeaveInfo> leaveInfos = leaveRequestDao.getLeaveInfo(empNbr, freq);
+
 		if (removeZeroedOutLeaveTypes) {
-			for(int i = 0; i < leaveInfos.size(); i++)
-			{
-				if(leaveInfos.get(i).getBeginBalance().doubleValue() == 0 &&
-					leaveInfos.get(i).getAdvancedEarned().doubleValue() == 0 &&
-					leaveInfos.get(i).getUsed().doubleValue() == 0 &&
-					leaveInfos.get(i).getPendingUsed().doubleValue() == 0 &&
-					leaveInfos.get(i).getPendingPayroll().doubleValue() == 0 &&
-					leaveInfos.get(i).getPendingEarned().doubleValue() == 0 &&
-					leaveInfos.get(i).getAvailableBalance().doubleValue() == 0 &&
-					leaveInfos.get(i).getPendingApproval().doubleValue() == 0)
-				{
+			for (int i = 0; i < leaveInfos.size(); i++) {
+				if (leaveInfos.get(i).getBeginBalance().doubleValue() == 0
+						&& leaveInfos.get(i).getAdvancedEarned().doubleValue() == 0
+						&& leaveInfos.get(i).getUsed().doubleValue() == 0
+						&& leaveInfos.get(i).getPendingUsed().doubleValue() == 0
+						&& leaveInfos.get(i).getPendingPayroll().doubleValue() == 0
+						&& leaveInfos.get(i).getPendingEarned().doubleValue() == 0
+						&& leaveInfos.get(i).getAvailableBalance().doubleValue() == 0
+						&& leaveInfos.get(i).getPendingApproval().doubleValue() == 0) {
 					leaveInfos.remove(i);
 					i--;
 				}
 			}
 		}
-		
+
 		return leaveInfos;
 	}
+
 	public void saveLvComments(BeaEmpLvComments comments) {
 		leaveRequestDao.saveLvComments(comments);
 	}
+
 	public void saveLvWorkflow(BeaEmpLvWorkflow flow, BhrEmpDemo demo) {
 		leaveRequestDao.saveLvWorkflow(flow);
-		//create alert
+		// create alert
 		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a E");
-		String message = sdf.format(new Date())+": Leave Request from "+ demo.getNameF()+" " + demo.getNameL() +" pending your approval";
-		alertDao.createAlert(demo.getEmpNbr(), flow.getApprvrEmpNbr(),message);
+		String message = sdf.format(new Date()) + ": Leave Request from " + demo.getNameF() + " " + demo.getNameL()
+				+ " pending your approval";
+		alertDao.createAlert(demo.getEmpNbr(), flow.getApprvrEmpNbr(), message);
 	}
+
 	public void deleteLeaveComments(BeaEmpLvComments comments) {
 		leaveRequestDao.deleteLeaveComments(comments);
 	}
+
 	public void deleteLeaveFlow(BeaEmpLvWorkflow flow) {
 		leaveRequestDao.deleteLeaveFlow(flow);
 	}
+
 	public BeaEmpLvRqst getBeaEmpLvRqstById(int id) {
 		return leaveRequestDao.getBeaEmpLvRqstById(id);
 	}
-	public List<BhrEmpLvXmital> getApprovedLeaves(String empNbr, String leaveType, String searchStart, String searchEnd, String freq) throws ParseException {
+
+	public List<BhrEmpLvXmital> getApprovedLeaves(String empNbr, String leaveType, String searchStart, String searchEnd,
+			String freq) throws ParseException {
 		// TODO Auto-generated method stub
-        SimpleDateFormat sdf1 = new SimpleDateFormat("MM/dd/yyyy");
-        SimpleDateFormat sdf2 = new SimpleDateFormat("yyyyMMdd");
-		List<BhrEmpLvXmital> result = leaveRequestDao.getApprovedLeaves(empNbr, leaveType, searchStart, searchEnd, freq);
-		for(int i=0;i<result.size();i++) {
+		SimpleDateFormat sdf1 = new SimpleDateFormat("MM/dd/yyyy");
+		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyyMMdd");
+		List<BhrEmpLvXmital> result = leaveRequestDao.getApprovedLeaves(empNbr, leaveType, searchStart, searchEnd,
+				freq);
+		for (int i = 0; i < result.size(); i++) {
 			result.get(i).getId().setDtOfPay(sdf1.format(sdf2.parse(result.get(i).getId().getDtOfPay())));
 			result.get(i).setDtOfAbs(sdf1.format(sdf2.parse(result.get(i).getDtOfAbs())));
 		}
