883678bdc622c9b6644b25ef5ecb15c2729edb2d
Wenhao1015 <lvwh@shinetechchina.com>
2019-03-13 16:01:10 +0800

    added customized logout success handler to set the district id as the user input from the corresponding login

---
+++ java/ESC20/src/main/java/com/esc20/security/CustomSuccessLogoutHandler.java
@@ -0,0 +1,29 @@
+package com.esc20.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import com.esc20.util.DataSourceContextHolder;
+
+public class CustomSuccessLogoutHandler implements LogoutSuccessHandler{
+
+	@Override
+	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		String database = DataSourceContextHolder.getDataSourceType();
+		database = database.split("/")[1];
+		database = database.substring(2);
+        String returnURL= "/"+request.getContextPath().split("/")[1]+"/login?distid=" + database;
+        request.getSession().setAttribute("districtId", database);
+        response.sendRedirect(returnURL);
+	}
+
+}
