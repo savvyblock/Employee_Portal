fd8d3c93fbe0a126e4888b4d2c35897dd5f66992
GillianChen <chenxz@shinetechchina.com>
2019-12-06 13:11:28 +0800

    changes back

--- java/ESC20/EmployeePortal/src/main/java/com/esc20/util/MailUtil.java
+++ java/ESC20/EmployeePortal/src/main/java/com/esc20/util/MailUtil.java
@@ -1,44 +1,3 @@
-//package com.esc20.util;
-//
-//import java.util.Properties;
-//
-//import javax.mail.Message;
-//import javax.mail.MessagingException;
-//import javax.mail.Session;
-//import javax.mail.Transport;
-//import javax.mail.internet.InternetAddress;
-//import javax.mail.internet.MimeMessage;
-//public class MailUtil {
-//
-//	private static final String serverHost = "smtp.163.com";
-//
-//	private static final Integer serverPort = 25;
-//
-//	private static final String fromAddress = "m15043019587@163.com";
-//	
-//	private static final String pass = "";
-//	
-//	public static void sendEmail(String to, String subject, String content) throws MessagingException{
-//		Properties props = new Properties();
-//		props.put("mail.smtp.host", serverHost);
-//		props.put("mail.smtp.port", serverPort);
-//		props.put("mail.smtp.auth", "true");
-//		props.put("mail.transport.protocol", "smtp");
-//		Session session = Session.getInstance(props);
-//		
-//		MimeMessage message = new MimeMessage(session);
-//		message.setSubject(subject);
-//		message.setContent(content,"text/html;charset=UTF-8");
-//		message.setFrom(new InternetAddress(fromAddress));
-//		message.setRecipients(Message.RecipientType.TO, to);
-//		Transport transport = session.getTransport();
-//		transport.connect(fromAddress, pass);
-//		transport.sendMessage(message, message.getAllRecipients());
-//		transport.close();
-//	}
-//	
-//}
-
 package com.esc20.util;
 
 import java.util.Properties;
@@ -52,24 +11,19 @@ import javax.mail.internet.MimeMessage;
 public class MailUtil {
 
 	//private static final String serverHost = "tcc11smtp.txeis.net";
-	private static final String serverHost = "smtp.163.com";
+	private static final String serverHost = "tcc20smtp.txeis.net"; 
 
 	private static final Integer serverPort = 25;
 
-	//private static final String fromAddress = "parentportal@txeis.net";
-	private static final String fromAddress = "chenxuezhen_1@163.com";
+	private static final String fromAddress = "employeeportal@txeis.net";
 	
 	public static void sendEmail(String to, String subject, String content) throws MessagingException{
-		to = "chenxuezhen_1@163.com";
 		Properties props = new Properties();
 		props.put("mail.smtp.host", serverHost);
 		props.put("mail.smtp.port", serverPort);
-		/*props.put("mail.smtp.auth", "false");
-		props.put("mail.transport.protocol", "smtp");
-		Session session = Session.getInstance(props);*/
-		
-		props.put("mail.smtp.auth", "true");
-		Session session  =  Session.getInstance(props,new MailAuthenticator(fromAddress,"Gillian84"));
+		props.put("mail.smtp.auth", "false");
+		//props.put("mail.transport.protocol", "smtp");
+		Session session = Session.getInstance(props);
 		
 		MimeMessage message = new MimeMessage(session);
 		message.setSubject(subject);
@@ -81,4 +35,5 @@ public class MailUtil {
 		transport.sendMessage(message, message.getAllRecipients());
 		transport.close();*/
 	}
+	
 }
