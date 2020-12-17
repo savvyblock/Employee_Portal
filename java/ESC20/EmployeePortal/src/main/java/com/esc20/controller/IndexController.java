0a031ddd82a286e2fb6fe90d8cf7d6f90f60fcbc
GillianChen <chenxz@shinetechchina.com>
2020-12-17 16:59:57 +0800

    ALC-13

--- java/ESC20/EmployeePortal/src/main/java/com/esc20/controller/IndexController.java
+++ java/ESC20/EmployeePortal/src/main/java/com/esc20/controller/IndexController.java
@@ -266,8 +266,8 @@ public class IndexController {
     //ALC-13 add method to check if the user exist or not
 	@RequestMapping(value = "isUserExisted", method = RequestMethod.POST)
 	@ResponseBody
-	public Map<String, Boolean> isUserExisted(String username) {
-		Map<String, Boolean> res = new HashMap<>();
+	public JSONObject isUserExisted(String username) {
+		JSONObject res = new JSONObject();
 		Boolean isExisted = false;
 		BeaUsers user = this.indexService.getUserByUsername(username);
 		if (user != null) {
